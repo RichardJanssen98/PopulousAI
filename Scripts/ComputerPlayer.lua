7590446160fff7f9646ebeaaa07253048cd5c96b
MrKosjaK <misterkosiak@gmail.com>
2021-02-10 18:27:05 +0300

    Ai can build huts.

--- Scripts/ComputerPlayer.lua
+++ Scripts/ComputerPlayer.lua
@@ -1,5 +1,38 @@
 local gs = gsi()
 
+_BUILD_BUFFER_IDXES = {
+  [0] = {},
+  [1] = {},
+  [2] = {},
+  [3] = {},
+  [4] = {},
+  [5] = {},
+  [6] = {},
+  [7] = {}
+}
+
+_SHAPE_HUTS_BUFFER = {
+  [0] = {},
+  [1] = {},
+  [2] = {},
+  [3] = {},
+  [4] = {},
+  [5] = {},
+  [6] = {},
+  [7] = {}
+}
+
+_NEW_HUTS_BUILT_BUFFER = {
+  [0] = {},
+  [1] = {},
+  [2] = {},
+  [3] = {},
+  [4] = {},
+  [5] = {},
+  [6] = {},
+  [7] = {}
+}
+
 ComputerPlayer = {}
 ComputerPlayer.__index = ComputerPlayer
 
@@ -9,13 +42,137 @@ function ComputerPlayer:Create(_PN)
   self.ERROR = false
   self.isActive = true
   self.PlayerNum = _PN or nil
-  self._Shapes = {}
+
+  --Building attributes
+  self.AttrMaxBldgsOnGoing = 0
+  self.AttrPrefHuts = 0
+  self.AttrPrefWarriorTrains = 0
+  self.AttrPrefFirewarriorTrains = 0
+  self.AttrPrefTempleTrains = 0
+  self.AttrPrefSpyTrains = 0
+
+  --Building flags
+  self.FlagsConstructBldgs = false
+  self.FlagsAutoBuild = false
 
   return self
 end
 
-function ComputerPlayer:ASDF()
-  -- i hate myself
+function ComputerPlayer:GetHutsCount()
+  return gs.Players[self.PlayerNum].NumBuiltOrPartBuiltBuildingsOfType[1] + gs.Players[self.PlayerNum].NumBuiltOrPartBuiltBuildingsOfType[2] + gs.Players[self.PlayerNum].NumBuiltOrPartBuiltBuildingsOfType[3] + #_SHAPE_HUTS_BUFFER[self.PlayerNum]
+end
+
+function ComputerPlayer:ProcessBuilding()
+  local pn = self.PlayerNum
+  if (#_NEW_HUTS_BUILT_BUFFER[pn] > 0) then
+    local t_bldg = _NEW_HUTS_BUILT_BUFFER[pn][1]
+    if (isFlagIdOn(t_bldg.u.Bldg.Flags,3)) then
+      if (OnBuildingComplete ~= nil and type(OnBuildingComplete) == 'function') then
+        CallHook(OnBuildingComplete, t_bldg)
+        table.remove(_NEW_HUTS_BUILT_BUFFER[pn], 1)
+      end
+    end
+  end
+  if (#_BUILD_BUFFER_IDXES[pn] > 0) then
+    local mapIdx = _BUILD_BUFFER_IDXES[pn][1]
+
+    if (mapIdx == nil) then
+      table.remove(_BUILD_BUFFER_IDXES[pn],1)
+      goto process_bldg_skip
+    end
+
+    if (self:GetHutsCount() < self.AttrPrefHuts) then
+      local buildable = true
+      local orient = G_RANDOM(4)
+      local mp_ent = MapPosXZ.new()
+      mp_ent.Pos = mapIdx
+      for f=0,1 do
+        increment_map_idx_by_orient(mp_ent,(2+orient) % 4)
+      end
+      local c2d = Coord2D.new()
+
+      map_idx_to_world_coord2d(mp_ent.Pos,c2d)
+      if (is_point_steeper_than(c2d,350) ~= 0) then
+        buildable = false
+        table.remove(_BUILD_BUFFER_IDXES[pn], 1)
+        goto process_bldg_skip
+      end
+      --local _c3d = Coord3D.new()
+      --map_idx_to_world_coord3d(idx,_c3d)
+      SearchMapCells(2,0,0,1,mapIdx,function(me)
+        if (not me.ShapeOrBldgIdx:isNull()) then
+          buildable = false
+          return false
+        end
+
+        --This doesn't work yet.
+        if (BUILD_FLAGS_CHECK_FOR_OBSTACLES) then
+          if (me.Flags & (1<<1) ~= 0) then
+            buildable = false
+            return false
+          end
+        end
+        --This doesn't work yet.
+
+        if (me.Flags & (1<<9) ~= 0 and me.Flags & (1<<10) ~= 0) then
+          buildable = false
+          return false
+        end
+        local l_idx = MAP_ELEM_PTR_2_IDX(me)
+        local a = is_map_cell_bldg_markable(gs.Players[pn],l_idx,0,0,1,0)
+        if (a == 0) then
+          buildable = false
+          return false
+        end
+        return true
+      end)
+      if (buildable) then
+        process_shape_map_elements(mapIdx, 1, orient, pn, 2)
+        createThing(7,2,8,MAP_XZ_2_WORLD_XYZ(mp_ent.XZ.X,mp_ent.XZ.Z),false,false)
+      end
+
+      table.remove(_BUILD_BUFFER_IDXES[pn], 1)
+    end
+
+    ::process_bldg_skip::
+  end
+end
+
+function ScanAreaForBldg(_pn, _idx, _radius)
+  local a = _radius
+  local m_idx = MapPosXZ.new()
+  m_idx.Pos = _idx
+  m_idx.XZ.X = m_idx.XZ.X-a
+  m_idx.XZ.Z = m_idx.XZ.Z-a
+  local x = 0
+  local z = 0
+  local num = 1
+  for i=1,a do
+    x = m_idx.XZ.X + (i*2)
+    for j=1,a do
+      z = m_idx.XZ.Z + (j*2)
+      if (i==1 or i==a or j==1 or j==a) then
+        local c_c3d = MAP_XZ_2_WORLD_XYZ(x,z)
+        local me = world_coord3d_to_map_ptr(c_c3d)
+        if (me.Flags & (1<<16) == 0 and me.Flags & (1<<9) == 0 and me.Flags & (1<<10) == 0 and me.Flags & (1<<18) == 0 and me.Flags & (1<<26) == 0 and me.Alt ~= 0) then
+          local d = createThing(7,3,0,c_c3d,false,false)
+          d.u.Effect.Duration = 32
+          table.insert(_BUILD_BUFFER_IDXES[_pn],world_coord3d_to_map_idx(c_c3d))
+        end
+      end
+    end
+  end
+end
+
+function AddShapeToQueue(t,pn,bldgModel)
+  if (bldgModel == 1) then
+    table.insert(_SHAPE_HUTS_BUFFER[pn], t)
+    log("ShapeHutsBufferCount: " .. #_SHAPE_HUTS_BUFFER[pn])
+  end
+end
+
+function AddBldgToQueue(t,pn)
+  table.insert(_NEW_HUTS_BUILT_BUFFER[pn], t)
 end
 
 -- Temporary, might change logic.
@@ -44,7 +201,7 @@ function ComputerPlayer:PreInitialize()
 
   --Mandatory to check if it's a function, you never know who is smart.
   if (OnPlayerInit ~= nil and type(OnPlayerInit) == 'function') then
-    CallHook(OnPlayerInit,self.PlayerNum)
+    CallHook(OnPlayerInit,self.PlayerNum,self)
   end
   ::preinit_end::
 end
--- Scripts/MainEntry.lua
+++ Scripts/MainEntry.lua
@@ -3,21 +3,28 @@ import(Module_String)
 import(Module_Globals)
 import(Module_DataTypes)
 import(Module_Package)
+import(Module_Map)
 import(Module_Table)
 import(Module_Objects)
 import(Module_Players)
+import(Module_Shapes)
+import(Module_Game)
 
 require "Mods\\PopulousAi\\Scripts\\Lib\\LibHooks"
 require "Mods\\PopulousAi\\Scripts\\Lib\\LibGameTurn"
 require "Mods\\PopulousAi\\Scripts\\Lib\\LibFlags"
 require "Mods\\PopulousAi\\Scripts\\ComputerPlayer"
 
-function OnPlayerInit(pn)
-  log(string.format("Player %d was initiated.", pn))
-end
-
 local gs = gsi()
 local gns = gnsi()
+
+function OnPlayerInit(pn,CP)
+  log(string.format("Player %d was initiated.", pn))
+  ScanAreaForBldg(pn, world_coord3d_to_map_idx(gs.Players[pn].ReincarnSiteCoord), 13)
+  ScanAreaForBldg(pn, world_coord3d_to_map_idx(gs.Players[pn].ReincarnSiteCoord), 15)
+  ScanAreaForBldg(pn, world_coord3d_to_map_idx(gs.Players[pn].ReincarnSiteCoord), 17)
+  CP.AttrPrefHuts = 25 + G_RANDOM(20)
+end
 
 local AiPlayers = {}
 
@@ -67,44 +74,82 @@ local DEBUG_STR = string.format("MainEntry.lua has been successfully loaded.")
 
 function OnTurn()
   local _TURN = GetTurn()
-  if isEvery2Pow(5) then
-    --Temporary
-    log ("Computer Players Count: " .. #AiPlayers)
+
+  --Ai's building entry point
+  if isEvery2Pow(1) then
     for i,CP in ipairs(AiPlayers) do
-      if (AiPlayers[i] == nil) then
-        log("it's nil!" .. i)
-        goto skip
-      end
-      local _S = CP:isValid()
-      local _STR = string.format("Is computer player valid? : %s", tostring(_S))
-      log(_STR)
-      if (not _S) then
-        log("Computer Player was removed. Num: " .. CP.PlayerNum)
-        log("" .. i)
-        --This is important to set it to nil, instead of table.remove()
-        AiPlayers[i] = nil
-      end
-      ::skip::
+      CP:ProcessBuilding()
     end
-    -- Unfortunately LUA doesn't pass variable as REFERENCE, so have to return flag from a function :/
-    -- But thats still looks cleaner. MACRO defines should work
-    gns.Flags = toggleFlag(gns.Flags,(1<<15))
-    local s = getShaman(0)
-    if (s ~= nil) then
-      if (isFlagIdOn(s.Flags2,25)) then
-        log("Is in balloon!")
-      end
-      if (isFlagOff(s.Flags,(1<<19))) then
-        log("Is in control!")
-      end
+  end
+  -- if isEvery2Pow(5) then
+  --   --Temporary
+  --   log ("Computer Players Count: " .. #AiPlayers)
+  --   for i,CP in ipairs(AiPlayers) do
+  --     if (AiPlayers[i] == nil) then
+  --       log("it's nil!" .. i)
+  --       goto skip
+  --     end
+  --     local _S = CP:isValid()
+  --     local _STR = string.format("Is computer player valid? : %s", tostring(_S))
+  --     log(_STR)
+  --     if (not _S) then
+  --       log("Computer Player was removed. Num: " .. CP.PlayerNum)
+  --       log("" .. i)
+  --       --This is important to set it to nil, instead of table.remove()
+  --       AiPlayers[i] = nil
+  --     end
+  --     ::skip::
+  --   end
+  --   -- Unfortunately LUA doesn't pass variable as REFERENCE, so have to return flag from a function :/
+  --   -- But thats still looks cleaner. MACRO defines should work
+  --   gns.Flags = toggleFlag(gns.Flags,(1<<15))
+  --   local s = getShaman(0)
+  --   if (s ~= nil) then
+  --     if (isFlagIdOn(s.Flags2,25)) then
+  --       log("Is in balloon!")
+  --     end
+  --     if (isFlagOff(s.Flags,(1<<19))) then
+  --       log("Is in control!")
+  --     end
+  --   end
+  --   for i=0,31 do
+  --     -- local _STR = string.format("Id: %d ; State: %s", i, tostring(isFlagIdOn(gns.Flags,i)))
+  --     -- log(_STR)
+  --     -- _STR = string.format("Id: %d ; State: %s", i, tostring(isFlagOn(gns.Flags2,(1<<i))))
+  --     -- log(_STR)
+  --   end
+  --   log(string.format("Turn: %d", GetTurn()))
+  -- end
+end
+
+local function DoesExist(input)
+  local res = false
+  local idx = nil
+  for i,k in ipairs(AiPlayers) do
+    if (k.PlayerNum == input) then
+      res = true
+      idx = i
+      break
     end
-    for i=0,31 do
-      -- local _STR = string.format("Id: %d ; State: %s", i, tostring(isFlagIdOn(gns.Flags,i)))
-      -- log(_STR)
-      -- _STR = string.format("Id: %d ; State: %s", i, tostring(isFlagOn(gns.Flags2,(1<<i))))
-      -- log(_STR)
+  end
+  return res, idx
+end
+
+function OnBuildingComplete(t)
+  ScanAreaForBldg(t.Owner, world_coord2d_to_map_idx(t.Pos.D2), 9)
+  ScanAreaForBldg(t.Owner, world_coord2d_to_map_idx(t.Pos.D2), 11)
+  ScanAreaForBldg(t.Owner, world_coord2d_to_map_idx(t.Pos.D2), 13)
+end
+
+function OnCreateThing(t)
+  if (t.Type == 9) then
+    AddShapeToQueue(t,t.Owner,t.u.Shape.BldgModel)
+  end
+
+  if (t.Type == 2) then
+    if (t.u.Bldg.HasBuildingExistedBefore == 0) then
+      AddBldgToQueue(t,t.Owner)
     end
-    log(string.format("Turn: %d", GetTurn()))
   end
 end
 
